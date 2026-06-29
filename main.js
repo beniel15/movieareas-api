98bbfb39ec82fcf49206a347732862bb7cc415a6
Beniel Raja <benielraja1503@gmail.com>
2026-06-29 14:08:07 +0530

    router proper formats

--- main.js
+++ main.js
@@ -6,6 +6,19 @@ app.get ("/",(req,res)=>{
         message: "Hello, World!"
     });  
 })
+//crud functionality of movies
+app.get('/movies',()=>{
+
+})
+app.post('/movies',()=>{
+
+})
+app.put('/movies/:id',()=>{
+
+})
+app.delete('/movies/:id',()=>{
+
+})
 app.listen(PORT,()=>{
     console.log(`The server is running at http://localhost:${PORT}`);
 });
